29acf4661f07273bd0769e7e22d2530b49823fb8
Revy <162366071+teoto4@users.noreply.github.com>
2024-04-12 03:38:45 +0300

    little_more to form

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const openForma = document.getElementById('open_forma');
 const closeForma = document.getElementById('forma_close');
 const forma = document.getElementById('forma');
 
+const disableBtn = document.getElementById('submit_disable');
+const validIn = document.getElementById('valid_in');
+
+disableBtn.setAttribute('disabled', true);
+
 openForma.addEventListener('click', function(e){
     e.preventDefault();
     forma.classList.add('active');
@@ -10,4 +15,12 @@ openForma.addEventListener('click', function(e){
 closeForma.addEventListener('click', ()=> {
     forma.classList.remove('active');
 })
+
+validIn.oninput = function(){
+    if (validIn.value.length < 8) {
+        disableBtn.setAttribute('disabled', true);
+        } else {  
+        disableBtn.removeAttribute('disabled');
+    }
+}
 
